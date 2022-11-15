2491fbf9d57ffb7000716ea03919d338299710cc
b0nz <elliantosarono.p@gmail.com>
2022-11-15 14:09:59 +0700

    add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+interface IPhoto {
+  id: number;
+  albumId: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
 }
 
-export default App
+function fetchPhotos(): Promise<IPhoto[]> {
+  return fetch("https://jsonplaceholder.typicode.com/photos").then((res) =>
+    res.json()
+  );
+}
+
+function Photos() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["photos"],
+    queryFn: fetchPhotos,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error instanceof Error) return <div>Error: {error?.message}</div>;
+
+  return (
+    <div className="img-container">
+      {data?.map((item) => (
+        <img key={item.id} src={item.url} />
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Photos />
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
